d7dec7455b9321f7505964cb738b4c1a62d80f40
mnamaral1 <139656712+mnamaral1@users.noreply.github.com>
2023-08-09 13:43:12 -0600

    alerta del div creada, trabajo finalizado

--- script.js
+++ script.js
@@ -1,3 +1,11 @@
-function myFunction() {
+function saludo() {
     alert('hola!');
 }
+
+document.addEventListener("DOMContentLoaded", function() {
+    const marco = document.getElementsByTagName('div')[0];
+
+    marco.addEventListener("click", function saludoDiv() {
+        alert("¡Hola! Soy el div");
+    });
+});
