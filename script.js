4997108f89847429e358468c7525df5da98a94f4
mnamaral1 <139656712+mnamaral1@users.noreply.github.com>
2023-08-09 14:17:04 -0600

    solucion para que el clic en el boton no dispare la alerta de clic en el div

--- script.js
+++ script.js
@@ -2,10 +2,25 @@ function saludo() {
     alert('hola!');
 }
 
-document.addEventListener("DOMContentLoaded", function() {
+// en esta version si se hace clic en el boton, salen las dos alertas:
+
+/* document.addEventListener("DOMContentLoaded", function() {
     const marco = document.getElementsByTagName('div')[0];
 
     marco.addEventListener("click", function saludoDiv() {
         alert("¡Hola! Soy el div");
+    });
+}); */
+
+// en esta version si se hace clic en el boton, solo sale la alerta especifica del boton:
+
+document.addEventListener("DOMContentLoaded", function() {
+    const marco = document.getElementsByTagName('div')[0];
+
+    marco.addEventListener("click", function(event) {
+        if (!event.target.matches('button')) {
+            // La función se ejecutará solo si el clic no fue en un botón dentro del div
+            alert("¡Hola! Soy el div");
+        }
     });
 });
